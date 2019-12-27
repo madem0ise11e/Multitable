99be84c3f06ebf1411a4dc6d7477ffe3df18b4c1
Tetiana Kizan <t.kizan@studenti.unina.it>
2019-12-27 21:23:51 +0100

    Created range 10

--- Multitable.c
+++ Multitable.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 int main() {
-  int n, i, range;
-  printf("Enter an integer: ");
-  scanf("%d", &n);
-  printf("Enter the range: ");
-  scanf("%d", &range);
-  for (i = 1; i <= range; ++i) {
-    printf("%d * %d = %d \n", n, i, n*i);
-  }
-  return 0;
+    int n, i;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    for (i = 1; i <= 10; ++i) {
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+    return 0;
 }
