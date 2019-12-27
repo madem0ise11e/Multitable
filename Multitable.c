580b51a6c11ac53a5e8cf80d037ddf38ceac757d
Tetiana Kizan <t.kizan@studenti.unina.it>
2019-12-27 21:10:47 +0100

    Adjust

--- Multitable.c
+++ Multitable.c
@@ -5,7 +5,7 @@ int main() {
   scanf("%d", &n);
   printf("Enter the range: ");
   scanf("%d", &range);
-  for (i = 1, i <= range; ++i) {
+  for (i = 1; i <= range; ++i) {
     printf("%d * %d = %d \n", n, i, n*i);
   }
   return 0;
